2a41a91e7f893386f9bbcb72e0443a26fe29ae6c
Seth Lachman <lachmanseth17@gmail.com>
2019-09-17 18:18:20 -0400

    Added authorization on login from token.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+      })
+      .catch(err => console.log(err));
+    setCredentials({ username: "", password: "" });
+  };
+
   return (
     <div>
-      <h2>Login Page</h2>
+      <h2>Login Here</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
