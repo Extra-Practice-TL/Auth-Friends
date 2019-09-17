ba62f307a5285c046a49265b2ca7706d998c7f1f
Seth Lachman <lachmanseth17@gmail.com>
2019-09-17 18:28:12 -0400

    Friends displaying.

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/friends")
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+  if (friends.length === 0) {
+    return (
+      <div>
+        <h2>Loading Friends....</h2>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>Friends List Page</h2>
+      <ul>
+        {friends &&
+          friends.map(friend => <li key={friend.id}>{friend.name}</li>)}
+      </ul>
     </div>
   );
 };
